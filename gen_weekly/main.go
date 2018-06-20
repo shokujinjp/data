45a0ac2ffd13f03b9a6e98ecc0c96865a5b06046
whywaita <whywaita@whywrite.it>
2018-06-21 06:19:30 +0900

    rename dir name

--- gen_weekly/main.go
+++ gen_weekly/main.go
@@ -28,14 +28,14 @@ var (
 )
 
 type Record struct {
-	Id          string `csv:"id"`
-	Name        string `csv:"name"`
-	Price       string `csv:"price"`
-	Category    string `csv:"category"`
-	DayStart    string `csv:"day_start"`
-	DayEnd      string `csv:"day_end"`
-	CanWeekday  string `csv:"can_weekday"`
-	Description string `csv:"description"`
+	Id          string `csv:"id" json:"id"`
+	Name        string `csv:"name" json:"name"`
+	Price       string `csv:"price" json:"price"`
+	Category    string `csv:"category" json:"category"`
+	DayStart    string `csv:"day_start" json":day_start"`
+	DayEnd      string `csv:"day_end" json:"day_end"`
+	CanWeekday  string `csv:"can_weekday" json:"can_weekday"`
+	Description string `csv:"description" json:"description"`
 }
 
 func (r *Record) MarshalString() string {
